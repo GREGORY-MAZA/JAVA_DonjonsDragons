8ff93073713a061004893b397c08ed6e20f7c4cf
Greg <maza.gregory46@gmail.com>
2021-04-28 17:39:24 +0200

    Feat 1st step

---
+++ src/menu/CreatePlayer.java
@@ -0,0 +1,23 @@
+package menu;
+import characters.Guerrier;
+
+public class CreatePlayer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Guerrier g = new Guerrier("Greg", 10,10);
+		System.out.println(g);
+		
+		
+		
+		System.out.println("**        **   ****      ********   ****    **  ******* ********    **   **  ******** ");
+		System.out.println(" **      **   **  **     **    **   ** **   **    **    **          **   **     **    ");
+		System.out.println("  **    **   **    **    ******     **  **  **    **    **    ****  *******     **    ");
+		System.out.println("   **  **   ** **** **   **    **   **   ** **    **    **      **  **   **     **    ");
+		System.out.println("    ****   **        **  **     **  **    ****    **    ***    ***  **   **     **    ");
+		System.out.println("     **   **          ** **      ** **     ***  ******* **********  **   **     **    ");
+	}
+
+}
+
+
